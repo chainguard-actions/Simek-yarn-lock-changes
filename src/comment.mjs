8b8ce8f6dd52561c24b23c2ce2235b01dfae6147
actionchain <actionchain@chainguard.dev>
2026-06-17 01:11:12 +0000

    Update Simek/yarn-lock-changes@v0.12.1

---
+++ src/comment.mjs
@@ -0,0 +1,48 @@
+import { markdownTable } from 'markdown-table';
+
+import { STATUS_ORDER, countStatuses } from './utils.mjs';
+
+const ASSETS_URL = {
+  ADDED: 'https://git.io/J38HP',
+  DOWNGRADED: 'https://git.io/J38ds',
+  REMOVED: 'https://git.io/J38dt',
+  UPDATED: 'https://git.io/J38dY'
+};
+
+const getStatusLabel = status =>
+  `[<sub><img alt="${status}" src="${ASSETS_URL[status]}" height="16" /></sub>](#)`;
+
+export const createTable = (lockChanges, groupByType = false, plainStatuses = false) =>
+  markdownTable(
+    [
+      ['Name', 'Status', 'Previous', 'Current'],
+      ...Object.entries(lockChanges)
+        .sort((a, b) =>
+          groupByType
+            ? STATUS_ORDER.indexOf(a[1].status) - STATUS_ORDER.indexOf(b[1].status) ||
+              a[0].localeCompare(b[0])
+            : a[0].localeCompare(b[0])
+        )
+        .map(([key, { status, previous, current }]) => [
+          '`' + key + '`',
+          plainStatuses ? status : getStatusLabel(status),
+          previous,
+          current
+        ])
+    ],
+    { align: ['l', 'c', 'c', 'c'], alignDelimiters: false }
+  );
+
+const createSummaryRow = (lockChanges, status) => {
+  const statusCount = countStatuses(lockChanges, status);
+  return statusCount ? [getStatusLabel(status), statusCount] : undefined;
+};
+
+export const createSummary = lockChanges =>
+  markdownTable(
+    [
+      ['Status', 'Count'],
+      ...STATUS_ORDER.map(status => createSummaryRow(lockChanges, status))
+    ].filter(Boolean),
+    { align: ['l', 'c'], alignDelimiters: false }
+  );
